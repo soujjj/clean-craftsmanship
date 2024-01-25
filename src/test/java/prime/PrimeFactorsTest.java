cf8cc6a3e34fa65d6d949873a1f7c7f1f09e6731
matsumoto <soujjj@gmail.com>
2024-01-25 17:15:58 +0900

    add PrimeFactorsTest

--- src/test/java/prime/PrimeFactorsTest.java
+++ src/test/java/prime/PrimeFactorsTest.java
@@ -2,6 +2,7 @@ package prime;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 
 import java.util.ArrayList;
@@ -12,9 +13,18 @@ public class PrimeFactorsTest {
   @Test
   public void factors() throws Exception {
     assertThat(factorsOf(1), is(empty()));
+    assertThat(factorsOf(2), contains(2));
+    assertThat(factorsOf(3), contains(3));
+    assertThat(factorsOf(9), contains(3, 3));
   }
 
   private List<Integer> factorsOf(int n) {
-    return new ArrayList<>();
+    ArrayList<Integer> factors = new ArrayList<>();
+    for (int divisor = 2; n > 1; divisor++) {
+      for(; n % divisor == 0; n /= divisor) {
+        factors.add(divisor);
+      }
+    }
+    return factors;
   }
 }

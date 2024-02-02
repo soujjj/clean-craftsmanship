92ec237ea374fb3b4647dd291c47e0d9cd90abea
matsumoto <soujjj@gmail.com>
2024-02-02 10:22:01 +0900

    fix bowling

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -1,9 +1,49 @@
 package bowling;
 
 public class Game {
-  public void roll(int pins) {}
+
+  private int rolls[] = new int[21];
+  private int currentRoll = 0;
+
+  public void roll(int pins) {
+    this.rolls[currentRoll++] = pins;
+  }
 
   public int score() {
-    return 0;
+    int score = 0;
+    int frameIndex = 0;
+    for (int frame = 0; frame < 10; frame++) {
+      if (isStrike(frameIndex)) {
+        score += 10 + strikeBonus(frameIndex);
+        frameIndex++;
+      } else if (this.isSpare(frameIndex)) {
+        score += 10 + spareBonus(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += twoBallsInFrame(frameIndex);
+        frameIndex += 2;
+      }
+    }
+    return score;
+  }
+
+  private int twoBallsInFrame(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex + 1];
+  }
+
+  private int spareBonus(int frameIndex) {
+    return rolls[frameIndex + 2];
+  }
+
+  private int strikeBonus(int frameIndex) {
+    return rolls[frameIndex + 1] + spareBonus(frameIndex);
+  }
+
+  private boolean isStrike(int frameIndex) {
+    return rolls[frameIndex] == 10;
+  }
+
+  private boolean isSpare(int frameIndex) {
+    return this.rolls[frameIndex] + rolls[frameIndex + 1] == 10;
   }
 }
--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -14,11 +14,56 @@ public class BowlingTest {
     this.game = new Game();
   }
 
+  /**
+   * @param n 投げる回数
+   * @param pins 倒れるピン数
+   */
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      this.game.roll(pins);
+    }
+  }
+
+  private void rollSpare() {
+    rollMany(2, 5);
+  }
+
+  private void rollStrike() {
+    this.game.roll(10);
+  }
+
   @Test
   public void gutterGame() throws Exception {
-    for (int i = 0; i < 20; i++) {
-      game.roll(1);
-    }
-    assertEquals(20, game.score());
+    rollMany(20, 0);
+    assertEquals(0, game.score());
+  }
+
+  @Test
+  public void allOnes() throws Exception {
+    rollMany(20, 1);
+    assertEquals(20, this.game.score());
+  }
+
+  @Test
+  public void oneSpare() throws Exception {
+    this.rollSpare(); // spare
+    this.game.roll(7);
+    rollMany(17, 0); // gutter
+    assertEquals(24, this.game.score());
+  }
+
+  @Test
+  public void oneStrike() throws Exception {
+    rollStrike();
+    this.game.roll(2);
+    this.game.roll(3);
+    rollMany(16, 0);
+    assertEquals(20, this.game.score());
+  }
+
+  @Test
+  public void perfectGame() throws Exception {
+    rollMany(12, 10);
+    assertEquals(300, this.game.score());
   }
 }
